2a6b7d0a56fab17578db30fe0618a203d660c975
Brigitte Michau <brigitte@fontera.com>
2014-11-20 09:05:46 +0200

    Initial Commit

---
+++ Help/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Help
+//
+//  Created by Brigitte Michau on 2014/11/20.
+//  Copyright (c) 2014 BrigitteMichau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Help/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Help
+//
+//  Created by Brigitte Michau on 2014/11/20.
+//  Copyright (c) 2014 BrigitteMichau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
